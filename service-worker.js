971ce2bfce96e12482ba250f0e9f986178c1987d
dh278 <dh278434@yahoo.co.jp>
2026-07-18 13:51:46 +0900

    service-worker.js を更新

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,17 @@
-self.addEventListener('install',e=>self.skipWaiting());
+// ==================================
+// NikkeiNewsPWA Ver.3
+// Service Worker
+// ==================================
+
+
+const CACHE_NAME =
+"nikkei-news-v3";
+
+
+// 初回キャッシュ対象
+
+const CACHE_FILES = [
+
+    "./",
+
+    "./index.html",
